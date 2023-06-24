94deb0c3621b1c08fea1e6c985bd929153b5f190
양해정 <127839004+Haejeong079@users.noreply.github.com>
2023-06-25 01:18:51 +0900

    Java Basic Algorithm 1

---
+++ test2.java
@@ -0,0 +1,28 @@
+public class test2{
+    public static void main(String[] args) {
+        
+        int[] num = {10,55,23,2,79,101,16,82,30,45};
+        
+        int min = num[0];
+        int max = num[0];
+
+        int minPos = 0;
+        int maxPos = 0;
+
+        for(int i = 1; i<num.length; i++){
+
+            if(min > num[i]){
+                min = num[i];
+                minPos = i+1;
+
+            }
+            if(max < num[i]){
+                max = num[i];
+                maxPos = i+1;
+            }
+        }
+        System.out.println("가장 작은 값은"+ min + " 이며 위치는 " + minPos + "입니다.");
+        System.out.println("가장 작은 값은"+ max + " 이며 위치는 " + maxPos + "입니다.");
+    }
+
+}
